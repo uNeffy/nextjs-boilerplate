6411b08ea56db627d3f52888f49804d93e2eafd3
Neffy <41371274+uNeffy@users.noreply.github.com>
2023-05-29 01:06:50 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,21 +5,7 @@ export default function Home() {
   return (
     <>
       <AlertBar />
-      <main className={styles.main}>
-        <div className={styles.fillScreen}>
-          <div>
-          <h1 className="text-3xl font-bold underline">Quality is what matters most</h1>
-          <p>
-            Neffy&apos;s Store is providing the safest and top-quality switches
-            and menus, offering affordable and cheapest prices in the entire
-            market.
-          </p>
-          <div>
-
-          </div>
-          </div>
-        </div>
-      </main>
+      
     </>
   );
 }
